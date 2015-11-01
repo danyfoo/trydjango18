3daac5359c9574333b3a591ad56c912cb64c309b
Daniel Rivera Guzman <drivera1@uabc.edu.mx>
2015-11-01 14:55:13 -0800

    Comentarios en el proyecto

--- trydjango18/newsletter/admin.py
+++ trydjango18/newsletter/admin.py
@@ -5,8 +5,8 @@ from .forms import SignUpForm
 from .models import SignUp
 
 class SignUpAdmin(admin.ModelAdmin):
-    list_display = ["__unicode__", "timestamp", "updated"]
-    form = SignUpForm
+    list_display = ["__unicode__", "timestamp", "updated"]  #Campos que se desplegaran al momento de ver la app desde el admin
+    form = SignUpForm   #Formulario que se utilizara para anadir a la DB
     #class Meta:
     #    model = SignUp
 
--- trydjango18/newsletter/forms.py
+++ trydjango18/newsletter/forms.py
@@ -4,19 +4,22 @@ from .models import SignUp
 
 class SignUpForm(forms.ModelForm):
     class Meta:
-        model = SignUp
-        fields = ['full_name', 'correo']
+        model = SignUp  #Modelo de datos a usar
+        fields = ['full_name', 'correo'] #Campos que contendra el formulario
         #exclude = ['full_name']    #NO RECOMENDABLE USARLO
 
-    def clean_full_name(self):
-        full_name = self.cleaned_data.get('full_name')
-        if not full_name:
-            raise forms.ValidationError("Please add a full_name")
+    #Metodos para validar el contenido del formulario
+    def clean_full_name(self):  #Por convencion se hace de esta manera: clean_<NOMBRE_DEL_CAMPO>
+        full_name = self.cleaned_data.get('full_name')  #Obtenemos el valor del campo
+        if not full_name:   #Encaso de que no haiga un full_name, se hace lo siguiente:
+            raise forms.ValidationError("Please add a full_name")   #raise publicara en el template un aviso
 
-        return full_name
+        return full_name #Regresamos ese nombre
 
+    #NO FUNCIONA CON email
     def clean_correo(self):
         correo = self.cleaned_data.get('correo')
+
         correo_base, provider = correo.split("@")
         domain, extension = provider.split('.')
         if not extension == 'edu':
